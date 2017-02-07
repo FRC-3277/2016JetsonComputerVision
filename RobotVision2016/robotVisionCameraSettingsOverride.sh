26185cd801944aa21eea47a23479c1a8155dd68d
FRC-3277 <prodigi3277@gmail.com>
2017-02-06 23:16:58 -0600

    Only a minor change...

--- RobotVision2016/robotVisionCameraSettingsOverride.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-v4l2-ctl -d /dev/video0 -c brightness=40
-v4l2-ctl -d /dev/video0 -c white_balance_temperature_auto=0
-v4l2-ctl -d /dev/video0 -c white_balance_temperature=2500
-v4l2-ctl -d /dev/video0 -c saturation=75
